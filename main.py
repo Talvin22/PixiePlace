56db5097079bad54ac4c93cbc3f0d4a85fb830c3
WorldRavenOfTigerTanks <133157853+WorldRavenOfTigerTanks@users.noreply.github.com>
2024-01-27 20:57:55 +0300

    testo

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 
 from Auth.auth import auth_router
+from Root.root import root_router
 from Models import models
 from database.database import engine
+
 
 app = FastAPI(
     title="PixiePlace"
 )
+
 models.Base.metadata.create_all(bind=engine)
 app.include_router(auth_router)
+app.include_router(root_router)
