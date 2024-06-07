d9712b73d7c40d229227336d3dee0225fe066055
SashaAndru <92024832+SashaAndru@users.noreply.github.com>
2024-06-06 22:25:11 -0700

    obnova0.3

---
+++ shop/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import footer from "./components/footer";
+function App() {
+  return (
+      <div className="wrapper">
+        <Header/>
+          <footer/>
+      </div>
+  );
+}
+
+export default App;
