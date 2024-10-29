155f9c047a11b2a56795ec93fbe4cb84c39178f4
SashaAndru <92024832+SashaAndru@users.noreply.github.com>
2024-10-29 17:17:34 +0200

    obnova 0.5

--- untitled/src/index.jsx
+++
@@ -1,8 +0,0 @@
-//import "/src/DivWrapper/global.css"
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import {DivWrapper} from "./DivWrapper";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<DivWrapper />)
